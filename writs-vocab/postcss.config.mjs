de40a5221550beba08fe1ed0ab72872422a29f8a
ajayratnan <65624062+ajayratnan@users.noreply.github.com>
2025-07-25 20:59:23 +0530

    Update postcss.config.mjs

--- writs-vocab/postcss.config.mjs
+++ writs-vocab/postcss.config.mjs
@@ -1,5 +1,8 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
+// postcss.config.js
+module.exports = {
+  plugins: {
+    // remove any nesting plugin entry here
+    'tailwindcss': {},
+    'autoprefixer': {},
+  },
+}
